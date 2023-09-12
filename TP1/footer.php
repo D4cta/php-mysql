e3c2f3a32093737315fbe95e7942f46771b77e19
Agathe DUPERRAY <ad539297@iut21.u-bourgogne.fr>
2023-09-12 16:10:52 +0200

    TP1

---
+++ TP1/footer.php
@@ -0,0 +1,8 @@
+<!-- footer.php -->
+<!-- footer.php -->
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3">
+    © 2021 Copyright:
+    <a class="text-dark">Alexandre Brabant</a>
+  </div>
+</footer>
