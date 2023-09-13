5d835901dfb13734b5f7ed6e843c245bb59bb6c5
Agathe DUPERRAY <ad539297@iut21.u-bourgogne.fr>
2023-09-13 11:07:16 +0200

    Ajout d'une clé password dans le tableau user de variables.php

--- TD2/variables.php
+++ TD2/variables.php
@@ -6,16 +6,19 @@ $users = [
         'full_name' => 'Mickaël Andrieu',
         'email' => 'mickael.andrieu@exemple.com',
         'age' => 34,
+        'password' => '123a',
     ],
     [
         'full_name' => 'Mathieu Nebra',
         'email' => 'mathieu.nebra@exemple.com',
         'age' => 34,
+        'password' => '123b',
     ],
     [
         'full_name' => 'Laurène Castor',
         'email' => 'laurene.castor@exemple.com',
         'age' => 28,
+        'password' => '123c',
     ],
 ];
 
