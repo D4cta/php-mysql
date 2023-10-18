6225f4daa63f0320bfba62078ad2c01224f1c604
Dacta <92741898+D4cta@users.noreply.github.com>
2023-10-18 14:50:49 +0200

    Ajout d'une clé password dans le tableau user de variable.php

--- TD 1/variables.php
+++ TD 1/variables.php
@@ -6,16 +6,19 @@ $users = [
         'full_name' => 'Mickaël Andrieu',
         'email' => 'mickael.andrieu@exemple.com',
         'age' => 34,
+        'password' => 'Mickaël'
     ],
     [
         'full_name' => 'Mathieu Nebra',
         'email' => 'mathieu.nebra@exemple.com',
         'age' => 34,
+        'password' => 'Mathieu'
     ],
     [
         'full_name' => 'Laurène Castor',
         'email' => 'laurene.castor@exemple.com',
         'age' => 28,
+        'password' => 'Laurene'
     ],
 ];
 
