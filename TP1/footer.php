a468358245b967942d212debf204f65ef469906a
Dacta <92741898+D4cta@users.noreply.github.com>
2023-10-18 14:14:47 +0200

    Problème de github, je remet mes TP et TD

--- TP1/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- footer.php -->
-<!-- footer.php -->
-<footer class="bg-light text-center text-lg-start mt-auto">
-  <div class="text-center p-3">
-    © 2021 Copyright:
-    <a class="text-dark">Alexandre Brabant</a>
-  </div>
-</footer>
