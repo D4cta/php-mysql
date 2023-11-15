a180fb940f61decd8f83503c5df298974dd0ff8c
Dacta <92741898+D4cta@users.noreply.github.com>
2023-11-15 11:32:12 +0100

    Mettre à jour le modèle

--- mvc-php/blog/src/model.php
+++ mvc-php/blog/src/model.php
@@ -23,3 +23,77 @@ function getPosts() {
 
     return $posts;
 }
+
+function getPost($identifier) {
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8',
+        'root', 'root');
+    } catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $statement = $database->prepare(
+        "SELECT id, title, content,
+        DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss')
+        AS french_creation_date FROM posts WHERE id = ?"
+    );
+
+    $statement->execute([$identifier]);
+    $row = $statement->fetch();
+
+    $post = [
+        'title' => $row['title'],
+        'french_creation_date' => $row['french_creation_date'],
+        'content' => $row['content'],
+    ];
+
+    return $post;
+}
+    
+
+function getComments($identifier){
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8',
+        'root', 'root');
+    } catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $statement = $database->prepare(
+        "SELECT id, author, comment,
+        DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss')
+        AS french_creation_date FROM comments WHERE post_id = ?
+        ORDER BY comment_date DESC"
+    );
+
+    $statement->execute([$identifier]);
+    Page 8
+    $comments = [];
+
+
+    while (($row = $statement->fetch())) {
+        $comment = [
+            'author' => $row['author'],
+            'french_creation_date' => $row['french_creation_date'],
+            'comment' => $row['comment'],
+        ];
+
+        $comments[] = $comment;
+    }
+
+    return $comments;
+}
+
+
+function dbConnect(){
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+    } 
+    
+    catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    return $database;
+}
+    
